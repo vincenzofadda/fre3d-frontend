706fcebc8372436107051b0da5f768e59ff42f95
João Victor <freitasjoaovictor49@gmail.com>
2023-11-18 13:04:25 -0300

    chore: refactoring project architecture

---
+++ src/pages/[...all].vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Página não encontrada
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'InitialPage',
+})
+
+// const name = ref('')
+
+// const router = useRouter()
+</script>
+
+<template>
+  <div>
+    FRE3D
+  </div>
+</template>

b402cdf566127704c85592022e645f5120cbf9cc
João Victor <freitasjoaovictor49@gmail.com>
2023-11-20 21:39:51 -0300

    feat: adding loading spinner component

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    LoadingSpinner: typeof import('./src/components/LoadingSpinner.vue')['default']
     Navbar: typeof import('./src/components/Navbar.vue')['default']
     RotatingCard: typeof import('./src/components/RotatingCard.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+interface Props {
+  color?: string
+  border?: string
+  size?: string[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: 'border-primary',
+  border: 'border-8',
+  size: () => ['w-12', 'h-12'],
+})
+</script>
+
+<template>
+  <!-- INNER SHADOW -->
+  <div class="relative">
+    <div
+      class="absolute border-gray-200 rounded-full border-solid"
+      :class="[props.size, props.border]"
+    />
+
+    <div
+      class="animate-spin border-t-transparent rounded-full border-solid shadow-md"
+      :class="[props.color, props.size, props.border]"
+    />
+  </div>
+</template>

e798b44fc2065038b6fdb61f3b59b6fb996574c4
João Victor <freitasjoaovictor49@gmail.com>
2023-11-21 10:34:25 -0300

    feat: adding pinia

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
+
 import Notifications from 'notiwind'
 import App from './App.vue'
+
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
 
 const app = createApp(App)
+const pinia = createPinia()
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 })
 
+pinia.use(piniaPluginPersistedState)
+
+app.use(pinia)
 app.use(router)
 app.use(Notifications)
 

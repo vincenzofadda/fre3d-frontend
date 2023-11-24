055c54f926ac74c4ea42f508418efec5d6e2507e
João Victor <freitasjoaovictor49@gmail.com>
2023-11-23 21:30:13 -0300

    refactor: adjusting navbar to reflect signed state

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 // @ts-expect-error this module does not have types
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import { useUserStore } from '../stores/user'
 
 defineOptions({
   name: 'Navbar',
 })
+
+const userStore = useUserStore()
+
+const loggedIn = computed(() => !!userStore.token)
 const router = useRouter()
 </script>
 
@@ -18,17 +23,21 @@ const router = useRouter()
     </router-link>
 
     <div flex items-center gap-x-4>
-      <button class="hover:text-primary flex gap-x-2 px-6 py-2 text-white transition">
+      <button class="flex gap-x-2 px-6 py-2 text-white transition hover:text-primary" @click="router.push('/carrinho')">
         <div i-carbon-shopping-cart />
         Carrinho
       </button>
 
       <button
-        class="hover:border-primary hover:bg-primary border border-white rounded-lg px-6 py-2 text-white transition"
+        v-if="!loggedIn"
+        class="border border-white rounded-lg px-6 py-2 text-white transition hover:border-primary hover:bg-primary"
         @click="router.push('/auth/login')"
       >
         Entrar
       </button>
+      <div cursor-pointer hover:text-underline>
+        {{ userStore.user?.name }}
+      </div>
     </div>
   </nav>
 </template>
